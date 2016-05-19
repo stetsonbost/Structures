1060d42fbbd9e5abf899e7a06baeb5796eadb388
stetsonbost <sbost@hmc.edu>
2016-05-18 23:58:12 -0700

    Implement several BasicBST functions, create framework for iterators

--- Trees/BasicBST-private.h
+++ Trees/BasicBST-private.h
@@ -5,10 +5,9 @@
  * \brief Implements the details of the BasicBST class
  */
 
-#include <math.h>
 #include <cassert>
 #include <cstddef>
-#include <string>
+// #include <string>
 
 template <class Item>
 BasicBST<Item>::BasicBST() : size_(0), root_(nullptr) {}
@@ -30,9 +29,9 @@ BasicBST<Item>::~BasicBST() {
 
 template <class Item>
 void BasicBST<Item>::destroyNode(Node& node) {
-  if node != nullptr {
-    destroyNode(node.left);
-    destroyNode(node.right);
+  if (node != nullptr) {
+    destroyNode(node->left_);
+    destroyNode(node->right_);
   }
   delete node;
   node = nullptr;
@@ -45,45 +44,127 @@ size_t BasicBST<Item>::size() const {
 
 template <class Item>
 void BasicBST<Item>::insert(const Item& value) {
-  insertNode(value,node);
+  insertNode(value, root_);
 }
 
 template <class Item>
 void BasicBST<Item>::insertNode(const Item& value, Node& node) {
-  if node == nullptr {
+  if (node == nullptr) {
     node = new Node{value,nullptr,nullptr};
     ++size_;
   }
-  if value < node.value_ {
-    insertNode(value, node.left);
+  if (value < node->value_) {
+    insertNode(value, node->left_);
   }
-  if value > node.value_ {
-    insertNode(value, node.right);
+  if (value > node->value_) {
+    insertNode(value, node->right_);
   }
-  // do nothing if value == node.value_ to avoid duplicates
+  // do nothing if value == node->value_ to avoid duplicates
 }
 
 template <class Item>
-Node* BasicBST<Item>::find(const Item& value) const {
-  findNode(value, root_);
+void BasicBST<Item>::erase(const Item& value) {
+	if (root_ != nullptr) {
+    eraseNode(value, root_);
+  }
 }
 
 template <class Item>
-Node* BasicBST<Item>::findNode(const Item& value, const Node& node)
+void BasicBST<Item>::eraseNode(const Item& value, Node*& node) {
+  if (value == node->value_) {
+    // TODO: the complicated deleting... many cases...
+    if (value == node->value_) {
+      if (node->left_ == nullptr && node->right_ == nullptr) {
+        //TODO: Check this??????????????
+        // set node's parent's left/right child to nullptr
+        if (node != root_) {
+          eraseChildNode(root_, node);
+        }
+        delete node;
+        node = nullptr;
+      } else {
+        if (node->left_ != nullptr) {
+          // TODO: replace node with node that is largest value in the left subtree
+        } else {
+          // TODO: replace node with node that is smallest value in the right subtree
+        }
+      }
+    }
+  } else {
+    if (value < node->value_ && node->left_ != nullptr) {
+      eraseNode(value, node->left_);
+    } 
+    if (value > node->value_ && node->right_ != nullptr) {
+      eraseNode(value, node->right_);
+    }
+  }
+}
+
+template <class Item>
+void BasicBST<Item>::eraseChildNode(Node*& parent, const Node*& child) const {
+  if (parent->left_ == child) {
+    parent->left_ == nullptr;
+  }
+  if (parent->right_ == child) {
+    parent->right_ == nullptr;
+  }
+
+}
+
+template <class Item>
+typename BasicBST<Item>::iterator BasicBST<Item>::find(const Item& value) const {
+  return findNode(value, root_);
+}
+
+template <class Item>
+typename BasicBST<Item>::iterator BasicBST<Item>::findNode(
+    const Item& value, const Node& node) const {
+  if (node == nullptr) {
+    return iterator(nullptr); // nullptr; // means value is not in subtree
+  }
+  if (value == node->value_) {
+    return iterator(node); // &node; // means value is in subtree
+  }
+  if (node < node->value_) {
+    return findNode(value, node->left_);
+  }
+  if (value > node->value_) {
+    return findNode(value, node->right_);
+  }
+}
+
+template <class Item>
+typename BasicBST<Item>::iterator BasicBST<Item>::begin() const {
+	if (root_ != nullptr) {
+    return beginNode(root_);
+  }
+  return iterator(nullptr);
+}
+
+template <class Item>
+typename BasicBST<Item>::iterator BasicBST<Item>::beginNode(const Node& node)
     const {
-  if node == nullptr {
-    return nullptr; // means value is not in subtree
+  if (node->left_ == nullptr) {
+    return iterator(node);
   }
-  if value == node.value {
-    return &node; // means value is in subtree
-  }
-  if node < node.value_ {
-    return findNode(value, node.left);
-  }
-  if value > node.value_ {
-    return findNode(value, node.right);
-  }
+  return beginNode(node->left_);
 }
+
+template <class Item>
+typename BasicBST<Item>::iterator BasicBST<Item>::end() const {
+	return iterator(nullptr);
+}
+
+// template <class Item>
+// const_iterator BasicBST<Item>::begin() const {
+// 	//TODO
+// }
+
+// template <class Item>
+// const_iterator BasicBST<Item>::end() const {
+// 	//TODO
+// }
+
 
 template <class Item>
 double BasicBST<Item>::calculateAverageNodeDepth() const {
@@ -93,4 +174,30 @@ double BasicBST<Item>::calculateAverageNodeDepth() const {
 template <class Item>
 int BasicBST<Item>::calculateHeight() const {
   //TODO
+}
+
+template <class Item>
+void BasicBST<Item>::outputAllValues() const {
+  if (root_ != nullptr) {
+    outputAllValuesNode(root_);
+  }
+}
+
+template <class Item>
+void BasicBST<Item>::outputAllValuesNode(const Node* node) const {
+  if (node->left_ != nullptr) {
+    outputAllValuesNode(node->left_);
+  }
+  std::cout << node->value_;
+  if (node->right_ != nullptr) {
+    outputAllValuesNode(node->right_);
+  }
+}
+
+// -------------------------------------
+// Implementation of BasicBST::Iterator
+// -------------------------------------
+
+template <class Item>
+BasicBST<Item>::Iterator::Iterator(const Node* node) : node_{node} {
 }
--- Trees/BasicBST.h
+++ Trees/BasicBST.h
@@ -11,11 +11,18 @@
 #define BASIC_BST_H
 #include <cstddef>
 #include <iostream>
-#include <string>
+// #include <string>
 
 template <class Item>
 class BasicBST {
+ private:
+  //template <class iteratorIsConst>
+  class Iterator; //<iteratorIsConst>;
+
  public:
+  using iterator = BasicBST::Iterator; // <false>;
+  // using const_iterator = Iterator<true>;
+
   /*
    * \brief Default constructor
    */
@@ -29,7 +36,7 @@ class BasicBST {
   /*
    * \brief Assignment operator
    */
-  BasicBST<Item> operator=(const BasicBST& tree) = delete;
+  BasicBST<Item> operator=(const BasicBST& tree);
 
   /*
    * \brief Default destructor
@@ -44,12 +51,30 @@ class BasicBST {
   /*
    * \brief Inserts key into the tree
    */
-  void insert(const Item& key);
+  void insert(const Item& value);
+  
+  /**
+   * \brief Erase element at iter
+   * \param iter iterator pointing to the character to erase ????????????????????
+   *
+   * \warning invalidates all iterators except the returned iterator ??????????????????????
+   */
+  void erase(const Item& value);
 
   /*
-   * \brief Inserts node into the tree
+   * \brief Finds a value in the tree
+   * \TODO: MAKE THIS RETURN A NODE* or ITERATOR POINTING TO THE FOUND LOCATION!!!!!!!!!
    */
-  Node* find(const Item& key) const;
+  iterator find(const Item& value) const;
+  
+  /// Return an iterator to the first element in the Tree
+  iterator begin() const;
+  /// Return an iterator to "one past the last"
+  iterator end() const;
+  // /// Return a const iterator to the first element in the Tree
+  // const_iterator begin() const;
+  // /// Return a const iterator to "one past the last"
+  // const_iterator end() const;
 
   /*
    * \brief Calculates average node depth of tree
@@ -61,11 +86,16 @@ class BasicBST {
    */
   int calculateHeight() const;
 
+  void outputAllValues() const;
+
  private:
-  struct Node {
+  class Node {
     Item value_;
     Node* left_;
     Node* right_;
+
+    friend class BasicBST;
+    friend class BasicBST<Item>::Iterator;
   };
 
   /*
@@ -77,13 +107,84 @@ class BasicBST {
    * \brief insert helper function
    */
   void insertNode(const Item& value, Node& node);
-
-  const Node* findNode(const Item& value, const Node& node);
+  void eraseNode(const Item& value, Node*& node);
+  void eraseChildNode(Node*& parent, const Node*& child) const;
+  iterator findNode(const Item& value, const Node& node) const;
+  iterator beginNode(const Node& node) const;
+  void outputAllValuesNode(const Node* node) const;
 
   /// size of the tree
   size_t size_;
   /// pointer to the root of the tree
   Node* root_;
+
+  // template <bool iteratorIsConst>
+  class Iterator {
+   public:
+    // Definitions that are required for this class to be a well-behaved
+    // STL-style iterator that moves forward through a collection of ints.
+    using value_type = Item;
+    using reference = value_type&;
+    using pointer = value_type*;
+    using difference_type = ptrdiff_t;
+    using iterator_category = std::forward_iterator_tag;
+
+    /*
+     * \brief Default constructor - disabled
+     */
+    Iterator() = default;
+    // Iterator(const Iterator&) = default;
+    // Iterator& operator=(const Iterator&) = default;
+    ~Iterator() = default;
+
+    /**
+     * This member function serves two purposes, depending on the type
+     * of this iterator.
+     *
+     * If this iterator is not a const_iterator
+     *  (i.e., iteratorIsConst is false), then this member function
+     *  is the copy constructor.
+     *
+     * If this iterator is a const_iterator (i.e., iteratorIsConst is true),
+     * then this member function converts a non-const iterator to a
+     * const_iterator.
+     */
+    Iterator(const BasicBST<Item>::iterator& iter) = default;
+
+    // // Make Iterator STL-friendly with these typedefs:
+    // using value_type = char;
+    // // The reference type depends (at compile-time!) on whether
+    // //  the iterator is a const_iterator or not.
+    // using reference =
+    //     typename std::conditional<iteratorIsConst, const value_type&,
+    //                               value_type&>::type;
+    // // The pointer type depends (at compile-time!) on whether
+    // //  the iterator is a const_iterator or not.
+    // using pointer =
+    //     typename std::conditional<iteratorIsConst, const value_type*,
+    //                               value_type*>::type;
+    // using difference_type = ptrdiff_t;
+    // using iterator_category = std::bidirectional_iterator_tag;
+    // using const_reference = const value_type&;
+
+    // // Operations
+    // Iterator& operator++();
+    // Iterator& operator--();
+    // reference operator*() const;
+    // bool operator==(const Iterator& rhs) const;
+    // bool operator!=(const Iterator& rhs) const;
+
+   private:
+    friend class BasicBST;
+
+    /// Parameterized Constructor
+    Iterator(const Node* node);
+
+    /// Private data members
+    Node* node_;
+    // Node* next_; // ??? maybe ???
+    // Node* prev_; // ??? maybe ???
+  };
 };
 
 #include "BasicBST-private.h"
