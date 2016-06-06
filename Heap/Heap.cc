5300378af75954e87a4ac32b0bb7304e67822e29
stetsonbost <sbost@hmc.edu>
2016-06-06 03:20:33 -0700

    Work on erase, alter parent and child functions

--- Heap/Heap.cc
+++ Heap/Heap.cc
@@ -51,14 +51,85 @@ Item& Heap<Item, isMaxHeap>::peekTop() const {
 
 template <class Item, bool isMaxHeap>
 void Heap<Item, isMaxHeap>::insert(const Item& value) {
+  // add value to bottom of heap
   data_->push_back(value);
-  if (true) {
-    //TODO
+  // get an iterator for the value
+  iterator valueIter = iterator(size() - 1, data_);
+
+  // if value is not at root, make an iterator pointing to value's parent
+  if (valueIter != begin()) {
+    iterator parentIter = parent(valueIter)
+
+    // Now move the value to the correct position in the Heap
+
+    if (isMaxHeap) {
+      // For Max Heap:
+
+      // if value is greater than parent, swap value and parent
+      while (*valueIter > *parentIter) {
+        swap(valueIter, parentIter);
+        valueIter = parentIter;
+        // if value is not at root, make update parent iterator
+        if (valueIter.index_ != begin()) {
+          parentIter = parent(valueIter);
+        } else {
+          // otherwise, the value is in the appropriate place
+          break;
+        }
+      }
+    } else {
+      // For Min Heap:
+
+      // if value is less than parent, swap value and parent
+      while (*valueIter < *parentIter) {
+        swap(valueIter, parentIter);
+        valueIter = parentIter;
+        // if value is not at root, make update parent iterator
+        if (valueIter.index_ != begin()) {
+          parentIter = parent(valueIter);
+        } else {
+          // otherwise, the value is in the appropriate place
+          break;
+        }
+      }
+    }
   }
 }
 
 template <class Item, bool isMaxHeap>
 void Heap<Item, isMaxHeap>::erase(iterator& iter) {
+  if (size() > 0) {
+    iterator lastIter = --end();
+    // swap value pointed to by iter with last element
+    swap(iter, lastIter);
+  }
+
+  // remove value (which must be at end)
+  data_->pop_back();
+
+  // Move values in heap to correct positions
+  // iter still points to same position but probably differnt value
+  if (size() > 1 && iter.index_ != size() - 1) {
+
+    if (isMaxHeap) {
+      // For Max Heap:
+
+      iterator lchild = lchild(iter);
+      iterator rchild = rchild(iter);
+      // stop if there are no children (rchild.data_ will also be nullptr)
+      if (lchild.data_ == nullptr) {
+        return;
+      }
+      // only has lchild 
+      // while *iter is less than either of it's children:
+      // TODO: need to deal with case where 1 or both children do not exist
+      // swap *iter with the lesser child
+
+    } else {
+      // For Min Heap:
+
+    }
+  }
 
 }
 
@@ -67,7 +138,7 @@ Item& Heap<Item, isMaxHeap>::removeTop() {
   // save value of top (to be returned)
   Item topValue = data_[0];
   // erase the top (includes readjusting heap structure)
-  iterator top = iterator(0);
+  iterator top = iterator(0, data_);
   erase(top);
   // return value of top
   return topValue;
@@ -104,43 +175,60 @@ void Heap<Item, isMaxHeap>::swap(iterator& value1, iterator& value2) {
 }
 
 template <class Item, bool isMaxHeap>
-typename Heap<Item, isMaxHeap>::iterator Heap<Item, isMaxHeap>::parent(iterator& child)
-    const {
-  size_t parent = ((child.index_ + 1) / 2) - 1;
-  return iterator(parent);
+typename Heap<Item, isMaxHeap>::iterator
+    Heap<Item, isMaxHeap>::parent(iterator& child)  const {
+  // if child is not top
+  if (child.index_ != 0) {
+    size_t parent = ((child.index_ + 1) / 2) - 1;
+    return iterator(parent, data_);
+  }
+  // if child is top
+  retun iterator(0, data_)
 }
 
 template <class Item, bool isMaxHeap>
-typename Heap<Item, isMaxHeap>::iterator Heap<Item, isMaxHeap>::lchild(iterator& parent)
-    const {
+typename Heap<Item, isMaxHeap>::iterator
+    Heap<Item, isMaxHeap>::lchild(iterator& parent) const {
   size_t lchild = (parent.index_ * 2) + 1;
-  return iterator(lchild);
+  // if lchild is valid
+  if (lchild < size()) {
+    return iterator(lchild, data_);
+  }
+  // if lchild is not valid
+  return iterator(0, nullptr);
 }
 
 template <class Item, bool isMaxHeap>
-typename Heap<Item, isMaxHeap>::iterator Heap<Item, isMaxHeap>::rchild(iterator& parent)
-    const {
+typename Heap<Item, isMaxHeap>::iterator 
+    Heap<Item, isMaxHeap>::rchild(iterator& parent) const {
   size_t rchild = (parent.index_ * 2) + 2;
-  return iterator(rchild);
+  // if rchild is valid
+  if (rchild < size()) {
+    return iterator(rchild, data_);
+  }
+  // if rchild is not valid
+  return iterator(0, nullptr);
 }
 
 
 template <class Item, bool isMaxHeap>
 typename Heap<Item, isMaxHeap>::iterator Heap<Item, isMaxHeap>::begin() const {
-  return iterator(0);
+  return iterator(0, data_);
 }
 
 template <class Item, bool isMaxHeap>
 typename Heap<Item, isMaxHeap>::iterator Heap<Item, isMaxHeap>::end() const {
-  return iterator(size());
+  return iterator(size(), data_);
 }
 
 template <class Item, bool isMaxHeap>
-typename Heap<Item, isMaxHeap>::const_iterator Heap<Item, isMaxHeap>::cbegin() const {
-  return const_iterator(0);
+typename Heap<Item, isMaxHeap>::const_iterator Heap<Item, isMaxHeap>::cbegin()
+    const {
+  return const_iterator(0, data_);
 }
 
 template <class Item, bool isMaxHeap>
-typename Heap<Item, isMaxHeap>::const_iterator Heap<Item, isMaxHeap>::cend() const {
-  return const_iterator(size());
+typename Heap<Item, isMaxHeap>::const_iterator Heap<Item, isMaxHeap>::cend()
+    const {
+  return const_iterator(size(), data_);
 }
